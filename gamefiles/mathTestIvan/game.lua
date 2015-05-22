a68194ebf34051e71e8e652c003c6b197b4b3d6a
Ivan0788 <ivan.zamora.vela@hotmail.com>
2015-05-21 19:49:57 -0500

    Finished Part
    Cleaned code, fruit image in boxes and matrix fruit distribution

--- gamefiles/mathTestIvan/game.lua
+++ gamefiles/mathTestIvan/game.lua
@@ -9,131 +9,30 @@ local settings = require( "settings" )
 
 local game = director.newScene() 
 ----------------------------------------------- Variables
-local answersLayer, wrongAnswersGroup
+local answersLayer
 local backgroundLayer
 local textLayer, instructions
 local manager
-local tapsEnabled
 local isFirstTime
---local gameTutorial
 local backgroundImage
 local instructionImage
---local wrongAnswers, correctAnswer
 local firstNumberLayout, secondNumberLayout, operation, equals, answer
 local number1, number2, result
 local answer_box1, answer_box2, answer_box3
 
 ----------------------------------------------- Constants
---local OFFSET_X_ANSWERS = 200
 local OFFSET_TEXT = {x = 0, y = -300}
-local SIZE_BOXES = 100
-local COLOR_WRONG = colors.red
-local COLOR_CORRECT = colors.green
-local WRONG_ANSWERS = 6
 local SIZE_FONT = 40
 local SCALE_BGX = 1.25
 local SCALE_IBI = 1.2
-local TIME_BOX_ANIMATION = 500
 local INSTRUCTION_TEXT = "Arrastra la respuesta correcta"
-local PADDING_WRONG_ANSWERS = 140
-local OFFSET_Y_WRONG_ANSWERS = 200
 local BOXES = 3
---local BOX_IMG_SIZE = 200
+local BOX_IMG_SIZE = 200
 local BOX_IMG_POSY = 225
---local BOX_SPACING_PADDING =  385
 local PADDING_ANSWERS = 300
---local FRUIT_SIZE = 75
---local OPERATION_SPACE = 5
---local OPERATION_PADDING = 200
-local TABLES = {
-	[1] = { {1} },
-	[2] = { {1,1} },
-	[3] = { {1,0,0} , 
-			{0,1,0} , 
-			{0,0,1} },
-	[4] = { {1,1} , 
-			{1,1} },
-	[5] = { {1,0,1} , 
-			{0,1,0} , 
-			{1,0,1} },
-	[6] = { {1,1,1} , 
-			{1,1,1} },
-	[7] = { {0,1,0} , 
-			{1,0,0} , 
-			{0,1,0} , 
-			{1,0,1} , 
-			{0,1,0} },
-	[8] = { {1,0,1} , 
-			{0,1,0} , 
-			{1,0,1} , 
-			{0,1,0} , 
-			{1,0,1} },
-	[9] = { {1,1,1} , 
-			{1,1,1} , 
-			{1,1,1} },
-}
+local OPERATION_PADDING = 225
 
 ----------------------------------------------- Functions
-local function onAnswerTapped(event)
-	local answer = event.target 
-	if tapsEnabled then
-		tapsEnabled = false 
-		if answer.isCorrect then 
-			if manager then 
-				manager.correct()
-			end
-		else
-			if manager then 
-				local correctGroup = display.newGroup() 
-				correctGroup.isVisible = false
-				
-				local box = display.newRect(0, 0, SIZE_BOXES, SIZE_BOXES)
-				box:setFillColor(unpack(COLOR_CORRECT))
-				correctGroup:insert(box)
-				
-				manager.wrong({id = "group", group = correctGroup}) 
-			end
-		end
-	end
-end
-
-local function removeDynamicAnswers()
-	display.remove(wrongAnswersGroup) 
-	wrongAnswersGroup = nil
-end
-
-local function createDynamicAnswers()
-	
-	removeDynamicAnswers() 
-	
-	wrongAnswersGroup = display.newGroup() 
-	answersLayer:insert(wrongAnswersGroup) 
-	
-	
-	local totalWidth = (WRONG_ANSWERS - 1) * PADDING_WRONG_ANSWERS
-	local startX = display.contentCenterX - totalWidth * 0.5 
-	
-	local function boxAnimation(box) 
-		local originalY = box.y
-		local targetY = originalY - 50
-		transition.to(box, {tag = scenePath, time = TIME_BOX_ANIMATION, y = targetY, transition = easing.outQuad, onComplete = function()
-			transition.to(box, {tag = scenePath, time = TIME_BOX_ANIMATION, y = originalY, transition = easing.inQuad, onComplete = function()
-				boxAnimation(box)
-			end})
-		end})
-	end
-	
-	for index = 1, WRONG_ANSWERS do
-		local wrongBox = display.newRect(startX + (index - 1) * PADDING_WRONG_ANSWERS, display.contentCenterY + OFFSET_Y_WRONG_ANSWERS, SIZE_BOXES, SIZE_BOXES)
-		wrongBox.isCorrect = false 
-		wrongBox:addEventListener("tap", onAnswerTapped)
-		wrongAnswersGroup:insert(wrongBox)
-		wrongBox:setFillColor(unpack(COLOR_WRONG)) 
-		boxAnimation(wrongBox)
-	end
-	
-	transition.to(wrongAnswersGroup, {tag = scenePath, time = 20000, alpha = 0.2}) 
-end
 
 local function initialize(event)
 	event = event or {} 
@@ -142,23 +41,14 @@ local function initialize(event)
 	isFirstTime = params.isFirstTime 
 	manager = event.parent 
 	
-	local operation = params.operation 
-	local wrongAnswers = params.wrongAnswers
-
 	instructions.text = INSTRUCTION_TEXT 
-end
-
-local function enableButtons()
-	tapsEnabled = true
-end
-
-local function disableButtons()
-	tapsEnabled = false
 end
 
 local function onTouchBox( event )
 	local object = event.target
-
+	local answerPosX, answerPosY = answer.x, answer.y
+	print("touch")
+	
 	if event.phase == "began" then
 		object.markX = object.x
 		object.markY = object.y
@@ -170,7 +60,10 @@ local function onTouchBox( event )
 		object.x, object.y = x, y
 	
 	elseif event.phase == "ended" then
-		if object.numChildren - 1 == result then 
+		local objPosX, objPosY = object.x, object.y
+		local distanceToPointX, distanceToPointY = math.abs( answerPosX - objPosX ), math.abs( answerPosY - objPosY )
+		
+		if object.total == result and distanceToPointX < 100 and distanceToPointY < 100 then 
 			object.x = answer.x
 			object.y = answer.y
 			
@@ -183,6 +76,11 @@ local function onTouchBox( event )
 		
 		display.getCurrentStage():setFocus(nil)
 	end
+	return true
+end
+
+local function onTapBox( event )
+	print("tap")
 	return true
 end
 
@@ -206,12 +104,28 @@ local function boxImage()
 end
 
 local function addFruit( num , num_Group )
-	for index = 1, num do
-		local sandia = display.newImage( assetPath.."sandia.png" ) 
-		sandia:scale(0.5,0.5) 
-		num_Group:insert( sandia )
-		print("addfruit "..num)
-	end
+	local startX = -65
+	local startY = -60
+	local totalSandia = 1
+	
+	num_Group.total = num
+	
+		for indexRow = 1, BOXES do
+			for indexColumns = 1, BOXES do
+				if totalSandia <= num then
+					local sandia = display.newImage( assetPath.."sandia.png")
+					sandia:scale(0.5,0.5) 
+					sandia.x = startX
+					sandia.y = startY
+					num_Group:insert( sandia )
+					startX = startX + sandia.contentWidth
+					totalSandia = totalSandia + 1
+					print("addfruit "..num)
+				end
+			end
+			startX = -65
+			startY = startY + 60
+		end
 end
 
 local function createBoxes( num , num_Group1 ,num_Group2 ,num_Group3)
@@ -228,6 +142,7 @@ local function createBoxes( num , num_Group1 ,num_Group2 ,num_Group3)
 			num_Group1.x = startX + (boxIndex - 1) * PADDING_ANSWERS
 			num_Group1.y = display.contentCenterY + BOX_IMG_POSY
 			num_Group1:addEventListener("touch", onTouchBox)
+			num_Group1:addEventListener("tap", onTapBox)
 			
 			if correctAnswer == 1 then
 				addFruit( num , num_Group1 )
@@ -244,6 +159,7 @@ local function createBoxes( num , num_Group1 ,num_Group2 ,num_Group3)
 			num_Group2.x = startX + (boxIndex - 1) * PADDING_ANSWERS
 			num_Group2.y = display.contentCenterY + BOX_IMG_POSY
 			num_Group2:addEventListener("touch", onTouchBox)
+			num_Group2:addEventListener("tap", onTapBox)
 			
 			if correctAnswer == 2 then
 				addFruit( num , num_Group2 )
@@ -260,6 +176,7 @@ local function createBoxes( num , num_Group1 ,num_Group2 ,num_Group3)
 			num_Group3.x = startX + (boxIndex - 1) * PADDING_ANSWERS
 			num_Group3.y = display.contentCenterY + BOX_IMG_POSY
 			num_Group3:addEventListener("touch", onTouchBox)
+			num_Group3:addEventListener("tap", onTapBox)
 			
 			if correctAnswer == 3 then
 				addFruit( num , num_Group3 )
@@ -280,18 +197,18 @@ local function problemLayout( sceneView )
 	firstNumberLayout.x = display.contentCenterX + -450
 	firstNumberLayout.y = display.contentCenterY - 125
 
-	operation = display.newImage( assetPath.."suma.png", firstNumberLayout.x + 225, firstNumberLayout.y, 200, 200 )
+	operation = display.newImage( assetPath.."suma.png", firstNumberLayout.x + OPERATION_PADDING, firstNumberLayout.y, BOX_IMG_SIZE, BOX_IMG_SIZE )
 	sceneView:insert( operation )
 	
 	secondNumberLayout = display.newGroup()
 	sceneView:insert( secondNumberLayout )
-	secondNumberLayout.x = operation.x + 225
+	secondNumberLayout.x = operation.x + OPERATION_PADDING
 	secondNumberLayout.y = operation.y
 	
-	equals = display.newImage( assetPath.."igual.png", secondNumberLayout.x + 225, secondNumberLayout.y, 200, 200 )
+	equals = display.newImage( assetPath.."igual.png", secondNumberLayout.x + OPERATION_PADDING, secondNumberLayout.y, BOX_IMG_SIZE, BOX_IMG_SIZE )
 	sceneView:insert( equals )
 	
-	answer = display.newImage( assetPath.."respuesta.png", equals.x + 225, equals.y, 200, 200 )
+	answer = display.newImage( assetPath.."respuesta.png", equals.x + OPERATION_PADDING, equals.y, BOX_IMG_SIZE, BOX_IMG_SIZE )
 	sceneView:insert( answer )
 	
 	answer_box1 = display.newGroup()
@@ -376,7 +293,7 @@ function game:show( event )
 		initialize(event)
 		problemLayout( sceneView ) 
 	elseif phase == "did" then 
-		--enableButtons()
+		
 	end
 end
 
@@ -385,6 +302,8 @@ function game:hide( event )
 	local phase = event.phase
 
 	if phase == "will" then 
+		
+	elseif phase == "did" then 
 		display.remove(firstNumberLayout)
 		firstNumberLayout = nil
 		
@@ -399,11 +318,6 @@ function game:hide( event )
 		
 		display.remove(answer_box3)
 		answer_box3 = nil
-		
-	elseif phase == "did" then 
-		
-		--disableButtons()
-		--transition.cancel(scenePath)
 	end
 end
 
