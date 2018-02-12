ccd23a60fe4d7d458c0d2d94a2a08ed6e4ae1cd2
mauvb <mau_v_b20@hotmail.com>
2018-02-12 09:48:10 -0600

    updated progTestMauricio
    Clouds effects added, van asset separated from background image, van with effect at end of game

--- gamefiles/progTestMauricio/game.lua
+++ gamefiles/progTestMauricio/game.lua
@@ -9,11 +9,13 @@ local game = director.newScene()
 local backgroundLayer, targetLayer, dynamicLayer
 local backgroundGroup, targetGroup, dynamicGroup
 local manager, isFirstTime
-local van, storedDevicesCounter, totalDevicesToSort, deviceToSort
-local tapsEnable
-local counterSwitchBoxSides, boxes
+local van, storedDevicesCounter, totalDevicesToSort
+local tapsEnabled, transitionsRunning
+local counterSwitchBoxSides, boxes, randomizedBoxData
 local currentLevel, elapsedGameTimer, currentLevelTime
 ----------------------------------------------- Constants
+local TRANSITION_TAG = "transitionTag"
+
 local DEVICES = {
 	[1] = {image = assetPath.."lampara.png", deviceType = "home"},
 	[2] = {image = assetPath.."licuadora.png", deviceType = "home"},
@@ -24,11 +26,17 @@ local DEVICES = {
 	[7] = {image = assetPath.."laptop.png", deviceType = "computing"},
 	[8] = {image = assetPath.."pc.png", deviceType = "computing"},
 	[9] = {image = assetPath.."robot.png", deviceType = "computing"},
-	[10] = {image = assetPath.."consola.png", deviceType = "computing"},
 	[11] = {image = assetPath.."calculadora.png", deviceType = "computing"},
+	[10] = {image = assetPath.."consola.png", deviceType = "computing"},
 	[12] = {image = assetPath.."z.png", deviceType = "computing"},
 	[13] = {image = assetPath.."registradora.png", deviceType = "computing"},
 	[14] = {image = assetPath.."atm.png", deviceType = "computing"}
+}
+
+local CLOUD_DATA = {
+	[1] = {image = assetPath..'nube.png', maxDelay = 1, minDelay = 0},
+	[2] = {image = assetPath..'nube.png', maxDelay = 12000, minDelay = 8000},
+	[3] = {image = assetPath..'nube.png', maxDelay = 22000, minDelay = 18000}
 }
 
 local LEVEL_DATA = {
@@ -37,9 +45,15 @@ local LEVEL_DATA = {
 	[3] = {availableTime = 45000, devicesToSort = 20, devicesPoolSize = 14}
 }
 
+
 local BOX_DATA = {
-	[1] = {id = "leftBox", x = display.contentWidth * 0.3, y = display.contentHeight * 0.8, width = 269, height = 200},
-	[2] = {id = "rightBox", x = display.contentWidth * 0.7, y = display.contentHeight * 0.8, width = 269, height = 200}
+	[1] = {id = "leftBox", x = display.contentWidth * 0.25, y = display.contentHeight * 0.8, width = 384, height = 285},
+	[2] = {id = "rightBox", x = display.contentWidth * 0.75, y = display.contentHeight * 0.8, width = 384, height = 285}
+}
+
+local BOX_ATTRIBUTES = {
+	[1] = {typeId = "computing", closedImageFill = {type = "image", filename = assetPath.."cajaPcClose.png"}, openedImageFill = {type = "image", filename = assetPath.."caja1.png"} },
+	[2] = {typeId = "home", closedImageFill = {type = "image", filename = assetPath.."cajaCasaClose.png"}, openedImageFill = {type = "image", filename = assetPath.."caja2.png"}}
 }
 ----------------------------------------------- Caches
 
@@ -50,39 +64,90 @@ local function validateManager(wonGame)
 	else
 		manager.wrong()
 	end
-  
-	timer.cancel(elapsedGameTimer)
-	elapsedGameTimer = nil
 end
 
-local function startTimer()
-	if currentLevel > 1 then
-		elapsedGameTimer = timer.performWithDelay(currentLevelTime, function () validateManager(false) end)
-	end
+local function startGameTimer()
+	elapsedGameTimer = currentLevel > 1 and timer.performWithDelay(currentLevelTime, function () 
+		if storedDevicesCounter ~= totalDevicesToSort then
+			validateManager(false) 
+		end
+	end)
 end
 
-local function shakeObject(touchedObject)
-	transition.to(touchedObject, {time = 70, rotation = 10, onComplete = function()
-		transition.to(touchedObject, {time = 70, rotation = -10, onComplete = function()
-			transition.to(touchedObject, {time = 70, rotation = 10, onComplete = function()
-				transition.to(touchedObject, {time = 70, rotation = -10, onComplete = function()
-					transition.to(touchedObject, {time = 70, rotation = 0})
+local function shakeObject(self)
+	transition.to(self, {tag = TRANSITION_TAG, time = 70, rotation = 10, onComplete = function()
+		transition.to(self, {tag = TRANSITION_TAG, time = 70, rotation = -10, onComplete = function()
+			transition.to(self, {tag = TRANSITION_TAG, time = 70, rotation = 10, onComplete = function()
+				transition.to(self, {tag = TRANSITION_TAG, time = 70, rotation = -10, onComplete = function()
+					transition.to(self, {tag = TRANSITION_TAG, time = 70, rotation = 0})
 				end})
 			end})
 		end})
 	end})
 end
 
+local function randomizeDataTable()
+	randomizedBoxData = BOX_ATTRIBUTES
+	
+	for dataTableId = 1, #BOX_ATTRIBUTES do
+		local randomId = math.random(#BOX_ATTRIBUTES)
+		randomizedBoxData[dataTableId], randomizedBoxData[randomId] = randomizedBoxData[randomId], randomizedBoxData[dataTableId]
+	end
+end
+
 local function sortBoxes()
 	local randomBox = math.random(1, 2)
 	local computingFill = {type = "image", filename = assetPath.."caja1.png"}
 	local homeFill = {type = "image", filename = assetPath.."caja2.png"}
+	randomizeDataTable()
+	
+	for totalBoxes = 1, #boxes do
+		boxes[totalBoxes].boxType, boxes[totalBoxes].fill = randomizedBoxData[totalBoxes].typeId, randomizedBoxData[totalBoxes].openedImageFill
+	end
+end
 
-	boxes[1].boxType = randomBox == 1 and "computing" or "home"
-	boxes[2].boxType = randomBox == 1 and "home" or "computing"
+local function boxesToVan(gameFinished)
+	local closedHomeBoxFill = {type = "image", filename = assetPath.."cajaCasaClose.png"}
+	local closedComputingBoxFill = {type = "image", filename = assetPath.."cajaPcClose.png"}
+	
+	if elapsedGameTimer then timer.pause(elapsedGameTimer) end
+	
+	for totalBoxes = 1, #boxes do
+		boxes[totalBoxes].fill = randomizedBoxData[totalBoxes].closedImageFill
+		
+		if totalBoxes ~= #boxes then 
+			transition.to(boxes[totalBoxes], {delay = 1000, tag = TRANSITION_TAG, time = 1000, xScale = 0.01, yScale = 0.01, alpha = 0, x = van.x, y = van.y, transition = easing.outBack})
+		else
+			transition.to(boxes[totalBoxes], {delay = 1000, tag = TRANSITION_TAG, time = 1000, xScale = 0.01, yScale = 0.01, alpha = 0, x = van.x, y = van.y, transition = easing.outBack, onComplete = function()
+		if not gameFinished then
+			sortBoxes()
+			
+			for boxesPositions = 1, #boxes do
+				boxes[boxesPositions].x, boxes[boxesPositions].y = BOX_DATA[boxesPositions].x, BOX_DATA[boxesPositions].y
+				
+				if boxesPositions ~= #boxes then
+					transition.to(boxes[boxesPositions], {tag = TRANSITION_TAG, time = 500, xScale = 1, yScale = 1, alpha = 1, transition = easing.outBack})
+				else
+					transition.to(boxes[boxesPositions], {tag = TRANSITION_TAG, time = 500, xScale = 1, yScale = 1, alpha = 1, transition = easing.outBack, onComplete = function()
+						van:generateNewDevice()
+					end})
+				end
+			end
+		else
+			van:removeSelf()
+			van = display.newImage(assetPath.."camionetaCerrada.png", display.contentCenterX, display.contentCenterY)
+			backgroundGroup:insert(van)
+			transition.to(van, {tag = TRANSITION_TAG, delay = 1000, time = 1000, xScale = 0.1, yScale = 0.1, alpha = 1, y = display.contentHeight * 0.45, onComplete = function()
+				validateManager(true)		
+			end})
+		end
+	end})
+		end
+	end
 
-	boxes[1].fill = randomBox == 1 and computingFill or homeFill
-	boxes[2].fill = randomBox == 1 and homeFill or computingFill
+	if elapsedGameTimer then timer.resume(elapsedGameTimer) end
+
+	return true
 end
 
 local function turnValidation(touchedObject)
@@ -90,22 +155,16 @@ local function turnValidation(touchedObject)
 	counterSwitchBoxSides = counterSwitchBoxSides + 1
 	
 	if storedDevicesCounter == totalDevicesToSort then
-		validateManager(true)
+		boxesToVan(true)
 	else
 		touchedObject:removeSelf()
 		if counterSwitchBoxSides == 5 and counterSwitchBoxSides ~= totalDevicesToSort then
+			tapsEnabled = false
 			counterSwitchBoxSides = 0
-			tapsEnable = false
-			transition.to(boxes[1], {time = 500, xScale = 0.01, yScale = 0.01, alpha = 0, transition = easing.outBack})
-			transition.to(boxes[2], {time = 500, xScale = 0.01, yScale = 0.01, alpha = 0, transition = easing.outBack, onComplete = function()
-				sortBoxes()
-				transition.to(boxes[1], {time = 500, xScale = 1, yScale = 1, alpha = 1, transition = easing.outBack})
-				transition.to(boxes[2], {time = 500, xScale = 1, yScale = 1, alpha = 1, transition = easing.outBack, onComplete = function()
-					tapsEnable = true
-				end})
-			end})
+			boxesToVan(false)
+		else
+			van:generateNewDevice()
 		end
-		van:generateNewDevice()
 	end
 end
 
@@ -132,8 +191,8 @@ local function checkBox(touchedObject)
 		and touchedObject.y <= boxes[boxIndex].contentBounds.yMax then
 			deviceTouchedBox = true
 			if touchedObject.deviceType == boxes[boxIndex].boxType then
-				transition.to(touchedObject, {time = 200, x = boxes[boxIndex].x, y = boxes[boxIndex].y, onComplete = function()
-					transition.to(touchedObject, {time = 300, xScale = 0.01, yScale = 0.01, y = boxes[boxIndex].y - boxes[boxIndex].contentHeight * 0.4, onComplete = function()
+				transition.to(touchedObject, {tag = TRANSITION_TAG, time = 200, x = boxes[boxIndex].x, y = boxes[boxIndex].y, onComplete = function()
+					transition.to(touchedObject, {tag = TRANSITION_TAG, time = 300, xScale = 0.01, yScale = 0.01, y = boxes[boxIndex].y - boxes[boxIndex].contentHeight * 0.4, onComplete = function()
 						turnValidation(touchedObject)
 					end})
 				end})
@@ -143,8 +202,8 @@ local function checkBox(touchedObject)
         
 				if correctBoxIndex > 0 then
 					shakeObject(touchedObject)
-					transition.to(touchedObject, {delay = 600, time = 1000, x = boxes[correctBoxIndex].x, y = boxes[correctBoxIndex].y, onComplete = function()
-						transition.to(touchedObject, {time = 300, xScale = 0.01, yScale = 0.01, y = boxes[correctBoxIndex].y - boxes[correctBoxIndex].contentHeight * 0.4, onComplete = function()
+					transition.to(touchedObject, {tag = TRANSITION_TAG, delay = 600, time = 1000, x = boxes[correctBoxIndex].x, y = boxes[correctBoxIndex].y, onComplete = function()
+						transition.to(touchedObject, {tag = TRANSITION_TAG, time = 300, xScale = 0.01, yScale = 0.01, y = boxes[correctBoxIndex].y - boxes[correctBoxIndex].contentHeight * 0.4, onComplete = function()
 							touchedObject:removeSelf()
 							van:generateNewDevice()
 						end})
@@ -158,30 +217,27 @@ local function checkBox(touchedObject)
 	end
   
 	if deviceTouchedBox == false then
-		transition.to(deviceToSort, {x = display.contentCenterX, y = display.contentHeight * 0.4, onComplete = function()
-			tapsEnable = true
+		transition.to(touchedObject, {tag = TRANSITION_TAG, x = display.contentCenterX, y = display.contentHeight * 0.4, onComplete = function()
+			tapsEnabled = true
 		end})
 	end
 end
 
 local function onObjectTouch(event)
-	if tapsEnable then
+	if tapsEnabled then
 		local touchedObject = event.target
     
-		if (event.phase == "began") then
+		if event.phase == "began" then
 			display.getCurrentStage():setFocus(touchedObject)
 			touchedObject.isFocus = true
-        
-		elseif (touchedObject.isFocus) then
-			if (event.phase == "moved") then
+		elseif touchedObject.isFocus then
+			if event.phase == "moved" then
 				touchedObject.x = event.x
 				touchedObject.y = event.y
- 
-			elseif (event.phase == "ended" or event.phase == "cancelled") then
- 
+			elseif event.phase == "ended" or event.phase == "cancelled" then
 				display.getCurrentStage():setFocus(nil)
 				touchedObject.isFocus = nil
-				tapsEnable = false
+				tapsEnabled = false
 				checkBox(touchedObject)
 			end
 		end
@@ -193,6 +249,11 @@ local function cleanUp()
 	if elapsedGameTimer then 
 		timer.cancel(elapsedGameTimer)
 		elapsedGameTimer = nil
+	end
+	
+	if transitionsRunning then
+		transition.cancel(TRANSITION_TAG)
+		transitionsRunning = false
 	end
   
 	backgroundGroup:removeSelf()
@@ -206,9 +267,8 @@ local function cleanUp()
 end
 
 local function createBoxes()
-	local creatingBox
 	for boxIndex = 1, #BOX_DATA do
-		creatingBox = display.newRect(BOX_DATA[boxIndex].x, BOX_DATA[boxIndex].y, BOX_DATA[boxIndex].width, BOX_DATA[boxIndex].height)
+		local creatingBox = display.newRect(BOX_DATA[boxIndex].x, BOX_DATA[boxIndex].y, BOX_DATA[boxIndex].width, BOX_DATA[boxIndex].height)
 		creatingBox.id = BOX_DATA[boxIndex].id
 		dynamicGroup:insert(creatingBox)
 		boxes[boxIndex] = creatingBox
@@ -220,27 +280,42 @@ end
 local function generateNewDevice()
 	local randomDevice = math.random(LEVEL_DATA[currentLevel].devicesPoolSize)
   
-	deviceToSort = display.newImage(DEVICES[randomDevice].image)
+	local deviceToSort = display.newImage(DEVICES[randomDevice].image)
     deviceToSort.deviceType = DEVICES[randomDevice].deviceType
     deviceToSort.alpha = 0
-    deviceToSort.x, deviceToSort.y = display.contentCenterX, display.contentHeight * 0.4
+    deviceToSort.x, deviceToSort.y = display.contentCenterX, display.contentHeight * 1.1
     deviceToSort:scale(0.01, 0.01)
-  
     targetLayer:insert(deviceToSort)
   
     deviceToSort:addEventListener("touch", onObjectTouch)
   
-    transition.to(deviceToSort, { time = 700, alpha = 1, xScale = 0.7, yScale = 0.7, transition = easing.outBack, onComplete = function()
-		tapsEnable = true
+    transition.to(deviceToSort, {tag = TRANSITION_TAG, time = 800, alpha = 1, xScale = 0.7, yScale = 0.7, y = display.contentHeight * 0.4, transition = easing.outBack, onComplete = function()
+		tapsEnabled = true
     end})
 end
 
 local function createVan()
-	van = display.newRect(display.contentCenterX, display.contentCenterY, 1, 1)
-	van.alpha = 0
+	van = display.newImage(assetPath.."camionetaAbiertaBlur.png", display.contentCenterX, display.contentCenterY)
 	backgroundGroup:insert(van)
 
 	van.generateNewDevice = generateNewDevice
+end
+
+local function animateCloud(cloud)
+	transition.to(cloud, {tag = TRANSITION_TAG, delay = math.random(cloud.minDelay, cloud.maxDelay), time = math.random(28, 32) * 1000, x = display.contentWidth * 1.1, onComplete = function()
+		cloud.x, cloud.y = display.contentWidth * -0.15 , display.contentHeight * 0.15
+		animateCloud(cloud)
+	end})
+end
+
+local function createClouds()	
+	for cloudId = 1, #CLOUD_DATA do
+		local cloudIndex = CLOUD_DATA[cloudId]
+		local cloud = display.newImage(cloudIndex.image, display.contentWidth * -0.15 , display.contentHeight * 0.15)
+		cloud.minDelay, cloud.maxDelay = cloudIndex.minDelay, cloudIndex.maxDelay
+		backgroundGroup:insert(cloud)
+		animateCloud(cloud)
+	end
 end
 
 local function createGroups()
@@ -257,20 +332,23 @@ end
 local function initialize(event)
 	event = event or {} 
 	local params = event.params or {}
+	local sceneParams = params.sceneParams or {}
   
 	isFirstTime = params.isFirstTime 
 	manager = event.parent 
   
 	math.randomseed(os.time())
   
-	tapsEnable = true
-  
-	currentLevel = 3
+	tapsEnabled = true
+	transitionsRunning = true
+	
 	boxes = {}
-	counterSwitchBoxSides = 0
+  
+	currentLevel = 1 --TODO: Replace with actual scenParams level.
 	storedDevicesCounter = 0
-	totalDevicesToSort = LEVEL_DATA[currentLevel].devicesToSort
+	counterSwitchBoxSides = 0
 	currentLevelTime = LEVEL_DATA[currentLevel].availableTime
+	totalDevicesToSort = LEVEL_DATA[currentLevel].devicesToSort
 end
 ----------------------------------------------- Module functions
 function game.getInfo() 
@@ -310,8 +388,9 @@ function game:show(event)
 	if phase == "will" then 
 		initialize(event)
 		createGroups()
+		createClouds()
 		createVan()
-		startTimer()
+		startGameTimer()
 		createBoxes()
 		van:generateNewDevice()
 	end
